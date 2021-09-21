c5fbc3162e52305473e84c13fb39467a979d4c7a
Gillian Lyle <gillian.lyle@outlook.com>
2021-09-21 22:12:24 +0100

    feat: initial tailwindcss setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], //remove unused styles in production
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
