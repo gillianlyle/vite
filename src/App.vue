c8345db7f55b02897e6cd51697510febed38e24f
Gillian Lyle <gillian.lyle@outlook.com>
2021-10-05 21:53:22 +0100

    feat: custom input component added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div :class="{ error: v$.firstName.$errors.length }">
+    <BaseInput 
+      id="firstName" 
+      class="input" 
+      v-model="firstName" 
+      label="First name" 
+      placeholder="Enter first name" 
+    />
+    <div class="input-errors" v-for="error of v$.firstName.$errors" :key="error.$uid">
+      <div class="error-msg">{{ error.$message }}</div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  padding:20px;
+<script>
+  import useVuelidate from '@vuelidate/core'
+  import { required, helpers } from '@vuelidate/validators'
+  import BaseInput from './components/BaseInput.vue';
+
+  export default {
+    name: 'HelloWorld',
+    components: {
+      BaseInput
+    },
+    setup () {
+      return { v$: useVuelidate() }
+    },
+    data () {
+      return {
+        firstName: '',
+      }
+    },
+    validations () {
+      return {
+        firstName: { required: helpers.withMessage('First name is required', required), $autoDirty: true, $lazy: true },
+      }
+    }
+  }
+
+</script>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.error-msg {
+  color:red;
 }
+
 </style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,33 @@
+<template>
+    <label :for="id">{{ label }}</label>
+    <input 
+        :id="id" 
+        class="base-input" 
+        :placeholder="placeholder" 
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)" />
+</template>
+<script>
+export default {
+    name: 'BaseInput',
+    props:{
+        id:{
+            type: String
+        },
+        label: {
+            type: String
+        },
+        placeholder: {
+            type: String
+        },
+        modelValue: {
+            type: String
+        }
+    },
+}
+</script>
+<style scoped>
+    .base-input {
+        @apply shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight;
+    }
+</style>
